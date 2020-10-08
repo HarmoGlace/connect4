e124f483e6773c044937098a2acbdba812436a24
HarmoGlace <harmoglacehelp@gmail.com>
2020-10-08 22:15:24 +0200

    Added cases and others players methods

--- connect4.py
+++ connect4.py
@@ -6,18 +6,29 @@ class Connect4Party:
 
         self.height = height
         self.width = width
+        self.default_color = '⚫'
+        self.cases = list(map(lambda position: { 'position': position, 'color': None }, range(0, height * width - 1)))
+
         self.__player_position = 0
 
     @property
     def current_player(self):
         return self.players[self.__check_position(self.__player_position)]
 
+    @property
+    def next_player(self):
+        return self.players[self.__check_position(self.__player_position + 1)]
+
     def __check_position(self, position):
         return position % len(self.players)
 
+    def increment_player(self):
+        self.__player_position = self.__check_position(self.__player_position + 1)
+        return self.current_player
+
     @property
     def results(self):
-        return self.data
+        return ''
 
     def __str__(self):
         return 'lol'
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from connect4 import Connect4Party
 
+lol = { 'mdr': 3 }
+
+print(lol)
+
 party = Connect4Party(['premier', 'deuxieme'])
 
-print(party.current_player)
+print(party.current_player, party.next_player)
+
+party.increment_player()
+
+print(party.current_player, party.next_player)
